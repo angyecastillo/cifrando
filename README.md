cfe7a52d195a94a63257225d988959f60f5c8372
angyecastillo <lindacasags@hotmail.com>
2018-06-18 12:25:03 -0500

    codigo cesar

---
+++ README.md
@@ -0,0 +1,10 @@
+# Cifrado César
+
+para poder utilizar este cifrado debes de escribir tu texto 
+despues debes de poner la llave con la cual quieras cifrar 
+
+
+en esta pagina hay un mensaje oculto deveras de encontrar la llave correcta para poder decifrarlo 
+
+te esperamos este sabado
+
---
+++ test/cipher.spec.js
@@ -0,0 +1,40 @@
+describe('cipher', () => {
+
+  it('debería ser un objeto', () => {
+    assert.equal(typeof cipher, 'object');
+  });
+ 
+  describe('cipher.encode', () => {
+ 
+    it('debería ser una función', () => {
+      assert.equal(typeof cipher.encode, 'function');
+    });
+ 
+    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33', () =>{
+        assert.equal(cipher.encode("ABCDEFGHIJKLMNOPQRSTUVWXYZ",33),"HIJKLMNOPQRSTUVWXYZABCDEFG");
+    });
+  });
+ 
+  describe('cipher.decode', () => {
+ 
+    it('debería ser una función', () => {
+      assert.equal(typeof cipher.decode, 'function');
+    });
+ 
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33', () =>{
+      assert.equal(cipher.decode("HIJKLMNOPQRSTUVWXYZABCDEFG",33),"ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    });
+  });
+ 
+  //Hacker edition! Intenta, aunque no es obligatorio
+  describe('cipher.createCipherWithOffset', () => {
+ 
+    it('debería ser una función', () => {
+      assert.equal(typeof cipher.createCipherWithOffset, 'function');
+    });
+ 
+    it('debería retornar un objeto con dos funciones (encode y decode) con offset fijado');
+ 
+  });
+ 
+ });
